79f1e323fa0fac841baf5106aea6c4ed589aa36c
Everton Souza <everton.lucas@academico.ifpb.edu.br>
2022-05-11 23:48:06 -0300

    Create ImprimeIntervalo.java
    questão 2

---
+++ ImprimeIntervalo.java
@@ -0,0 +1,12 @@
+class ImprimeIntervalo {
+        public static void main(String[] args) {
+            int i = 1;
+            long j = 2;
+            float num0 = 20.0f; 
+            double num1 = 30.0d;
+            boolean b = true;
+            char c = k;
+           
+            }
+        }
+    }
